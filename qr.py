e97e450d44c19db513fcbe589c3194bf50843530
Marc <114660717+Markuxcu@users.noreply.github.com>
2023-05-09 10:39:06 +0200

    qr.py

--- qr.py
+++ qr.py
@@ -1,13 +1,44 @@
 import qrcode
 
-with open('qr_aim.txt', 'r') as file:
-    data = file.read().replace('\n', '')
+choice = input("What should be encoded, a message(1) or an image(2):\n")
 
-qr = qrcode.QRCode()
-qr.add_data(data)
+def messageqr():
+	msgfile = input("\nEnter path to the file which contains your message:\n")
+	with open(msgfile, 'r') as file:
+		data = file.read().replace('\n', '')
+	
+	qr = qrcode.QRCode(
+		version=None,
+    		error_correction=qrcode.constants.ERROR_CORRECT_L,
+    		box_size=10,
+    		border=4,
+	)
+	qr.add_data(data)
 
-qr.make()
-nameqr = input('Name of the QR-Code:\n')
+	qr.make()
+	nameqr = input('Name of the QR-Code:\n')
 
-img = qr.make_image()
-img.save(nameqr  + '.png')
+	img = qr.make_image()
+	img.save(nameqr  + '.png')
+
+def picqr():
+	imgpath = input("\nEnter path to image you want to encode:\n")
+	img = Image.open(imgpath)
+	qr = qrcode.QRCode(
+    		version=None,
+    		error_correction=qrcode.constants.ERROR_CORRECT_L,
+    		box_size=10,
+    		border=4,
+	)
+	qr.add_data(img)
+	qr.make(fit=True)
+	img_qr = qr.make_image(fill_color="black", back_color="white")
+	namepicqr = input("\nName of the QR-Code:\n")
+	img_qr.save(namepicqr + ".png")
+    
+if choice == "1":
+        messageqr()
+elif choice == "2":
+        picqr()
+else:
+        print("\nPlease enter a valid choice (1 or 2)!")
